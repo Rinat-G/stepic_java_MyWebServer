22a018ef7a91bbe2a817d28740fa52a18c8ffc4b
Гималетдинов Ринат Рафаилович <SBT-Gimaletdinov1-RR@sigma.sbrf.ru>
2017-06-15 19:16:35 +0500

    SrvConfig impl

---
+++ src/main/java/accounts/AccountServiceInterface.java
@@ -0,0 +1,7 @@
+package accounts;
+
+/**
+ * Created by R0cky on 15.06.2017.
+ */
+public class AccountServiceInterface {
+}
---
+++ src/test/java/configuration/SrvConfigTest.java
@@ -0,0 +1,32 @@
+import configuration.SrvConfig;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by sbt-gimaletdinov1-rr on 14.06.2017.
+ */
+public class SrvConfigTest {
+
+    @Test
+    public void testGetInstance() throws IOException{
+        Object srvConfigInstance = SrvConfig.getInstance();
+        assertEquals(srvConfigInstance.getClass(), SrvConfig.class );
+        assertEquals(srvConfigInstance, SrvConfig.getInstance());
+    }
+
+    @Test
+    public void testGetProperty() throws IOException{
+        String value = SrvConfig.getInstance().getProperty("port");
+        assertEquals("8080", value);
+
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void testGetPropertyUncheckedException() throws Exception {
+        String value = SrvConfig.getInstance().getProperty("Some_sring");
+
+    }
+}
